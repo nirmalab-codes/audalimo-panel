4278e7ed5927b5e1e2b2f463338ebad585f7b982
raymond <raymond@stackless.id>
2024-07-15 22:21:05 +0700

    fix auth

--- src/layouts/full/vertical-sidebar/profile/Profile.vue
+++ src/layouts/full/vertical-sidebar/profile/Profile.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
 import { useAuthStore } from '@/stores/auth';
 
+import VueAvatar from "@webzlodimir/vue-avatar";
+import "@webzlodimir/vue-avatar/dist/style.css";
+import { storeToRefs } from 'pinia';
+
 const authStore = useAuthStore();
+const { user } = storeToRefs(authStore)
 </script>
 
 <template>
     <v-sheet rounded="md" color="lightsecondary" class="px-4 py-3 ExtraBox">
         <div class="d-flex align-center hide-menu">
             <v-avatar size="40">
-                <img src="@/assets/images/profile/user-1.jpg" alt="user" height="40" />
+                <!-- <img src="@/assets/images/profile/user-1.jpg" alt="user" height="40" /> -->
+                <vue-avatar :username="user.fullname" />
             </v-avatar>
             <div class="ml-4">
-                <h4 class="mb-n1 text-h6 textPrimary">Mathew</h4>
-                <span class="text-subtitle-2 textSecondary">Designer</span>
+                <h4 class="mb-n1 text-h6 textPrimary">{{ user.fullname }}</h4>
             </div>
             <div class="ml-auto">
                 <v-btn variant="text" icon rounded="md" color="primary" @click="authStore.logout()">
